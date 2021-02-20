46ec8f06ab7e7f8869001b4c4d2f9ed37a0d58f5
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-20 23:57:40 +0300

    Create inputfile.java

---
+++ token/inputfile.java
@@ -0,0 +1,12 @@
+import.os
+import.stream
+import.element
+     INIT == MAIN.CLASS
+      @Ovveride
+        stream(init.soicalView)
+       init.soical( ==> file.git
+     MAIN == CLASS STRUCTURE
+        init.element
+      socialView.git
+   
+        
