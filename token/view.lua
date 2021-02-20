1e6e3b57dad0c814d6e6ba007328d7b6cef0ed32
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-20 23:53:54 +0300

    Update view.lua

--- token/view.lua
+++ token/view.lua
@@ -13,6 +13,20 @@ language.file
     soical.view( ==> thisComamnd.lua
       command.string 
        file( "[git]" )
+  SocialView( == && git.lua
+     print(StringElement.blog)
+      this.view
+          file.index
+       file( "[string.blog]" )
+             blog.element( ==  }
+     lic( element.blog( == soical.git
+         blog.social( == printer
+            git.file( ={ api.soical
+         social.git( == share.element
+           enterText.t
 
+     
+
+ 
    
 
