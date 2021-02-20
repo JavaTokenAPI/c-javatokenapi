663c5c3535de78bb6ede311152bccb92ed841d1d
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-20 23:36:32 +0300

    Create tokens.h

---
+++ tokens.h
@@ -0,0 +1,25 @@
+#include <token>
+#include <main>
+  open.class(init.function)
+    language.source
+  print(importCilent).Language
+  print(importCilent).Language
+    class.var(value( == language.file
+    init.cilent(main ==> files
+    
+   folder =={ "TOKENS FILE"}
+     varServer =={ " CILENT FILE"}
+      this.cilent(Id( == && and.main(class.token
+   language.source
+  print(SocialView).Language
+  print(SocialView).Language
+   //BLOCKS//
+       folder == {"ImportCilent.SocialView" }
+       token ==  main.file
+           SoicalView.language( == SoicalMediaApp
+       CILENT.( == MySoicalView
+          social.view( {"createASocialExtension" }
+      this.cilent{ "nameCilent.id
+       CILENT.create( =="print.id"
+
+    
