ad0e50842fec5256a03c60fce66c1e7ab2fc3af2
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-20 23:49:54 +0300

    Create view.lua

---
+++ token/view.lua
@@ -0,0 +1,18 @@
+local language == ong
+local file == obj
+
+language.file
+   MakeMain.insert
+       Main ==> files
+  index.files ==> string.file
+    insert.lua( == K
+     file( "[[=]]"
+     index( "index]"
+        file.string.language
+       SoicalView.api
+    soical.view( ==> thisComamnd.lua
+      command.string 
+       file( "[git]" )
+
+   
+
